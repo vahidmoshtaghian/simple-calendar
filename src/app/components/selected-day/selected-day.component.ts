ebd5fa6e821b8a289039f03f6ef50b44dd2399c9
Vahid <vahid.moshtagh@gmail.com>
2024-11-28 19:13:23 +0330

    create hours

--- src/app/components/selected-day/selected-day.component.ts
+++ src/app/components/selected-day/selected-day.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-selected-day',
   templateUrl: './selected-day.component.html',
-  styleUrls: ['./selected-day.component.scss']
+  styleUrls: ['./selected-day.component.scss'],
+  imports: [CommonModule]
 })
 export class SelectedDayComponent implements OnInit {
   constructor() { }
@@ -12,4 +14,10 @@ export class SelectedDayComponent implements OnInit {
   ngOnInit() {
   }
 
+  clockHours = Array.from({ length: 24 }, (_, i) => {
+    const hour = i === 0 || i === 12 ? 12 : i % 12;
+    const period = i < 12 ? 'AM' : 'PM';
+
+    return `${hour} ${period}`;
+  });
 }
